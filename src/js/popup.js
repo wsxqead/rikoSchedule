49e1a770cdcb036525188347a87441f276107933
wsxqead <lsj122993@gmail.com>
2025-02-15 17:01:58 +0900

    openPopup 중복 의심 제거

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,9 @@
-function openPopup(event) {
-  const popup = document.getElementById("popup");
-  document.getElementById("event-title").textContent = event.event;
-  document.getElementById("event-images").innerHTML = `<img src="${event.image}" />`;
-  popup.style.display = "flex";
-}
+// function openPopup(event) {
+//   const popup = document.getElementById("popup");
+//   document.getElementById("event-title").textContent = event.event;
+//   document.getElementById("event-images").innerHTML = `<img src="${event.image}" />`;
+//   popup.style.display = "flex";
+// }
 
 function closePopupModal() {
   document.getElementById("popup").style.display = "none";
