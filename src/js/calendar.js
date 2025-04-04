3b7440198f399254e1ef5d198908c7cd018ecb6f
wsxqead <lsj122993@gmail.com>
2025-04-05 07:34:32 +0900

    feat:3월 일정 추가 등록

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -15,6 +15,7 @@ const availableMonths = [
   { year: 2025, month: "jan", label: "2025년 1월" },
   { year: 2025, month: "feb", label: "2025년 2월" },
   { year: 2025, month: "mar", label: "2025년 3월" },
+  { year: 2025, month: "apr", label: "2025년 4월" },
 ];
 
 // 연도 및 월 선택 박스 초기화
